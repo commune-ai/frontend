3e4da59692d18bf0b229b42d130986d365c8e78c
Luca Vivona <lucavivona01@gmail.com>
2023-07-09 12:47:19 -0400

    [update] 🛠️ fixed iframe module tool

--- src/pages/modules.tsx
+++ src/pages/modules.tsx
@@ -6,24 +6,15 @@ import '../css/global.css';
 export default function Modules() {
   return (
     <Layout dark-theme="dark" title={`🚀 Modules`}>
-      <div className='text-lg dark:text-white text-black'>
-        <div className='flex flex-col items-center justify-center h-screen'>
-          <div className='z-10 flex flex-col items-center justify-center rounded-lg mt-10'>
-            <h1 className='text-4xl font-bold'>
-              <span className='text-[#ffb4ed] dark:text-[#FFD6F5] hover:animate-pulse duration-500'>
-                commune
-              </span>::Modules 🚀
-
-              
-            </h1>
-            <img src="./gif/cubes/MultiColourCubeSpin.gif" className='w-[500px] h-[300px]' />
-            <p className='text-xl font-medium'>Coming soon... 🚧 🏗️</p>
-            <iframe
-          src='https://localhos:8501'  // Replace with the URL of the desired iframe content
-          width='800'
-          height='600'
-          title='Embedded Content'
+      <div className='text-lg dark:text-white text-black '>
+        <div className='flex flex-col items-center justify-center'>
+        <iframe
+            className='w-screen h-screen'
+            src='http://localhost:8501/?embed=true'  // Replace with the URL of the desired iframe content
+            title='Embedded Content'
         ></iframe>
+          <div className='fixed z-10 flex flex-col items-center justify-center rounded-lg mt-10'>
+           
           </div>
         </div>
       </div>
@@ -31,3 +22,13 @@ export default function Modules() {
     </Layout>
   );
 }
+
+{/* <h1 className='text-4xl font-bold'>
+<span className='text-[#ffb4ed] dark:text-[#FFD6F5] hover:animate-pulse duration-500'>
+  commune
+</span>::Modules 🚀
+
+
+</h1>
+<img src="./gif/cubes/MultiColourCubeSpin.gif" className='w-[500px] h-[300px]' />
+<p className='text-xl font-medium'>Coming soon... 🚧 🏗️</p> */}
