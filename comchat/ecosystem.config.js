81779b61dcaf8f664e82b62d66b5661d5e16f212
tunki1201 <tunki1201@gmail.com>
2024-01-26 00:24:00 -0500

    '[merge_with_backend_and_frontend]'

--- comchat/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [{
-        name: "com-chat",
-        script: "./server.js",
-        env: {
-            NODE_ENV: "production",
-        },
-    }]
-};
--- comchat/src/common/util/useToggleableBoolean.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-export function useToggleableBoolean(initialValue: boolean = false) {
-  const [value, setValue] = React.useState<boolean>(initialValue);
-  return { on: value, toggle: () => setValue(!value) };
-}
-
-export type ToggleableBoolean = ReturnType<typeof useToggleableBoolean>;
