5765f3f6ea1dd62c469741901c8c9e93e26e5895
tunki1201 <tunki1201@gmail.com>
2024-02-15 10:41:39 -0500

    [replaced the navbar]

--- frontend/src/app/telemetry/page.tsx
+++ frontend/src/app/telemetry/page.tsx
@@ -3,12 +3,12 @@ export default function TelemetryPage() {
     return (
         <div className='flex flex-col items-center justify-center my-auto'>
             <h1 className='text-4xl font-bold'>
-            <span className='text-[#ffb4ed] dark:text-[#FFD6F5] hover:animate-pulse duration-500'>
-            commune
-            </span>::Telemetry ⛓️
+                <span className='text-[#ffb4ed] dark:text-[#FFD6F5] hover:animate-pulse duration-500'>
+                    commune
+                </span>::Telemetry ⛓️
             </h1>
-            <img src="/gif/cubes/MultiColourCubeSpin.gif" className='w-[500px] h-[300px]' />
-            <p className='text-xl font-medium'>Coming soon...</p> 
+            {/* <img src="/gif/cubes/MultiColourCubeSpin.gif" className='w-[500px] h-[300px]' /> */}
+            <iframe src="http://telemetry.communeai.net/" style={{ width: '100%', height: '620px' }}></iframe>
         </div>
     );
 }
