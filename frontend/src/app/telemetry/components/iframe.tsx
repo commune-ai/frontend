1c6d51f87f655128cd64c153ffd58120972e6d79
dev3 <dev3@dev3s-MacBook-Air.local>
2024-02-15 18:27:02 +0100

    update links

---
+++ frontend/src/app/telemetry/components/iframe.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const iFrameTelemetry = () => {
+  return (
+    <>
+      <iframe src="http://telemetry.communeai.net/#/0xbc6eb9753e2417476601485f9f8ef8474701ec199d456f989bd397682c9425c5" title="Telemetry Iframe" style={{ height: "1000px", width: "100%", border: "none", overflow: "hidden" }}></iframe>
+    </>
+  );
+};
