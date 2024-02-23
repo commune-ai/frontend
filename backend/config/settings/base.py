e0d43da5dd55ba2a834ebacee84a833f7afbeece
tunki1201 <tunki1201@gmail.com>
2024-02-23 14:11:03 -0500

    [updated with backend]

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -47,7 +47,6 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
