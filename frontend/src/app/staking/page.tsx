062de380c9c83cbc4c3e1f99bf9fcf5b2fbe888c
tunki1201 <tunki1201@gmail.com>
2024-03-18 21:45:13 -0400

    [updated the frontend ui]

--- frontend/src/app/staking/page.tsx
+++ frontend/src/app/staking/page.tsx
@@ -1,19 +1,20 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import { Card, Button, Popover } from 'antd';
+import classnames from 'classnames';
+import { Card, Button } from 'antd';
 import { Pagination } from 'antd';
+import Style from './staking.module.css';
 import { ValidatorType } from '../api/staking/type';
 import { useGetValidatorsQuery } from "@/app/api/staking/modulelist"
 import SearchBar from '@/components/molecules/search-bar/search-bar';
 
 const CommunalSystem: React.FC = () => {
 
-    const { data: validatorData, isLoading: validatorLoading } =
+    const { data: validatorData } =
         useGetValidatorsQuery(undefined, {
             pollingInterval: 300000,
         })
 
-    console.log('-------------this is the validator--------', validatorData, validatorLoading)
     const [searchString, setSearchString] = useState("");
     const [stakedModules, setStakedModules] = useState<string[]>([]);
     const [currentPage, setCurrentPage] = useState(1);
@@ -82,45 +83,16 @@ const CommunalSystem: React.FC = () => {
                 {
                     displayedModules && displayedModules.map(
                         module => {
-                            const content = <div className='flex flex-col'>
+                            return <Card title={<span className={classnames('text-white')}>{module.name}</span>} style={{ width: 300, marginBottom: 20 }} className={classnames(Style.cardClass, 'mx-5 cursor-pointer shadow-xl card-class text-white')} key={module.address}>
+                                <p className='text-white'>Amount: {module.balance}</p>
+                                <p className='text-white'>Staked: {stakedModules.includes(module.address) ? 'Yes' : 'No'}</p>
                                 <div className='flex'>
-                                    <p>- Address: {module.address},</p>
-                                    <p className='ml-1'>- Apy: {module.apy},</p>
-                                    <p className='ml-1'>- Balance: {module.balance},</p>
-                                    <p className='ml-1'>- DelegationFee: {module.delegation_fee}</p>
-                                </div>
-                                <div className='flex'>
-                                    <p>- Dividends: {module.dividends},</p>
-                                    <p className='ml-1'>- Emission: {module.emission},</p>
-                                    <p className='ml-1'>- Incentive: {module.incentive},</p>
-                                    <p className='ml-1'>- Verified: {module.isVerified}</p>
-                                </div>
-                                <div className='flex'>
-                                    <p>- Key: {module.key},</p>
-                                    <p className='ml-1'>- Latest Update: {module.last_update},</p>
-                                    <p className='ml-1'>- Name: {module.name},</p>
-                                    <p className='ml-1'>- RegBlock: {module.regblock}</p>
-                                </div>
-                                <div className='flex'>
-                                    <p>- Stake: {module.stake},</p>
-                                    <p className='ml-1'>- TotalStakers: {module.total_stakers},</p>
-                                    <p className='ml-1'>- TotalValidators: {module.total_validators},</p>
-                                    <p className='ml-1'>- Type: {module.type},</p>
-                                    <p className='ml-1'>- WalletStaked: {module.wallet_staked}</p>
-                                </div>
-                            </div>;
-                            return <Popover content={content} title="Detail Info" key={module.address}>
-                                <Card title={module.name} style={{ width: 300, marginBottom: 20 }} className='mx-5 cursor-pointer shadow-xl'>
-                                    <p>Amount: {module.balance}</p>
-                                    <p>Staked: {stakedModules.includes(module.address) ? 'Yes' : 'No'}</p>
-                                    <div className='flex'>
-                                        <Button onClick={() => stakeModule(module.address)} className='flex items-center mr-1 bg-blue-500 rounded-lg shadow-lg hover:shadow-2xl text-center hover:bg-blue-600 duration-200 text-white hover:text-white font-sans 
+                                    <Button onClick={() => stakeModule(module.address)} className='flex items-center mr-1 bg-blue-500 rounded-lg shadow-lg hover:shadow-2xl text-center hover:bg-blue-600 duration-200 text-white hover:text-white font-sans 
                 justify-center px-2 py-2 cursor-pointer'>Stake</Button>
-                                        <Button onClick={() => unstakeModule(module.address)} className='mr-1'>Unstake</Button>
-                                        <Button onClick={() => useModule(module.address)}>Use</Button>
-                                    </div>
-                                </Card>
-                            </Popover>
+                                    <Button onClick={() => unstakeModule(module.address)} className='mr-1 text-white'>Unstake</Button>
+                                    <Button onClick={() => useModule(module.address)} className='text-white'>Use</Button>
+                                </div>
+                            </Card>
                         }
                     )
                 }
